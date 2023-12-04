ee525e905adcfebc17683081b966e6331e90a03d
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2023-12-04 11:22:03 +0100

    Create day4_p1.py

---
+++ 2023/day4/day4_p1.py
@@ -0,0 +1,19 @@
+# input = 'sample.txt'
+input = 'puzzle.txt'
+
+def main():
+  result_sum = 0
+  with open(input) as f:
+    for line in f:
+      count = -1
+      winning, mine = line[:-1].split(':')[1].split('|')
+      winning_set = set([int(num) for num in ' '.join(winning.split()).split()])
+      mine = [int(num) for num in ' '.join(mine.split()).split()]
+      for num in mine:
+        if(num in winning_set):
+          count += 1
+      result_sum += 2**count if count > -1 else 0
+  print(result_sum)
+  
+if __name__ == "__main__":
+  main()
