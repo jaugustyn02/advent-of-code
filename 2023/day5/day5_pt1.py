435f8b219053ed640117432afed082e9649d7d22
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2023-12-05 12:11:04 +0100

    day5: pt2 not finished

---
+++ 2023/day5/day5_pt1.py
@@ -0,0 +1,33 @@
+input = 'data/sample.txt'
+# input = 'data/puzzle.txt'
+
+
+def main():
+    with open(input) as f:
+        lines = f.readlines()
+        seeds = list(map(int, lines[0].split(': ')[1].split()))
+        seed_was_mapped: list[bool] = [False] * len(seeds)
+        for line in lines[2:]:
+            if line == '\n':
+                continue
+            if not line[0].isnumeric():
+                for i in range(len(seeds)):
+                    seed_was_mapped[i] = False
+                continue
+            
+            dest_range, src_range, range_len = map(int, line.split())
+            for i, seed in enumerate(seeds):
+                if not seed_was_mapped[i] and src_range <= seed < src_range + range_len:
+                    # print(seeds[i], seed - src_range + dest_range)
+                    seeds[i] = seed - src_range + dest_range
+                    seed_was_mapped[i] = True
+            
+
+        # for i, seed in enumerate(seeds):
+        #     seeds[i] = seed_map.get(seed, seed)
+            
+        print(min(seeds))
+        
+        
+if __name__ == '__main__':
+    main()
---
+++ 2023/day5/day5_pt2.py
@@ -0,0 +1,53 @@
+input = 'data/sample.txt'
+# input = 'data/puzzle.txt'
+
+
+def main():
+    with open(input) as f:
+        lines = f.readlines()
+        nums = list(map(int, lines[0].split(': ')[1].split()))
+        seed_ranges = [[nums[i], nums[i] + nums[i+1], False] for i in range(0, len(nums), 2)]
+        for line in lines[2:]:
+            if line == '\n':
+                print(seed_ranges)
+                continue
+            if not line[0].isnumeric():
+                for i in range(len(seed_ranges)):
+                    seed_ranges[i][2] = False
+                continue
+            
+            dest_start, map_start, range_len = map(int, line.split())
+            dest_end = dest_start + range_len
+            map_end = map_start + range_len
+            starts_diff = dest_start - map_start
+            for i, value in enumerate(seed_ranges):
+                src_start, src_end, was_mapped = value
+                # print(src_start, src_end, was_mapped)
+                if was_mapped:
+                    continue
+                if map_start <= src_start < map_end:
+                    seed_ranges[i][0] = dest_start
+                    seed_ranges[i][2] = True
+                    if map_end < src_end:
+                        seed_ranges[i][1] = map_end + starts_diff
+                        seed_ranges.append([map_end, src_end, False])
+                    else:
+                        seed_ranges[i][1] = dest_end
+                elif map_start < src_end <= map_end:
+                    seed_ranges[i][1] = dest_end
+                    seed_ranges[i][2] = True
+                    if src_start < map_start:
+                        seed_ranges[i][0] = map_start + starts_diff
+                        seed_ranges.append([src_start, map_start, False])
+                    else:
+                        seed_ranges[i][0] = dest_start
+                # if not seed_was_mapped[i] and src_range <= seed < src_range + range_len:
+                    # seeds[i] = seed - src_range + dest_range
+                    # seed_was_mapped[i] = True
+            
+            
+        print(min(seed_ranges, key=lambda x: x[0]))
+        
+        
+if __name__ == '__main__':
+    main()
