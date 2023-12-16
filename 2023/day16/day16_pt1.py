2376d07014aa404268177f1dfeaacb8312f8e6c7
Jan Augustyn <jaugustyn@student.agh.edu.pl>
2023-12-16 11:40:44 +0100

    day 16

---
+++ 2023/day16/day16_pt1.py
@@ -0,0 +1,111 @@
+from enum import Enum
+from collections import deque
+
+# input = 'data/sample.txt'
+input = 'data/puzzle.txt'
+
+
+class Direction(Enum):
+    UP = 0
+    RIGHT = 1
+    DOWN = 2
+    LEFT = 3
+
+    def to_vector(self):
+        if self == Direction.UP:
+            return (-1, 0)
+        elif self == Direction.RIGHT:
+            return (0, 1)
+        elif self == Direction.DOWN:
+            return (1, 0)
+        elif self == Direction.LEFT:
+            return (0, -1)
+    
+    def turn_left(self):
+        return Direction((self.value - 1) % 4)
+    
+    def turn_right(self):
+        return Direction((self.value + 1) % 4)
+
+beam_mirrors = {
+    '/': {
+        Direction.UP: Direction.RIGHT,
+        Direction.RIGHT: Direction.UP,
+        Direction.DOWN: Direction.LEFT,
+        Direction.LEFT: Direction.DOWN
+    },
+    '\\': {
+        Direction.UP: Direction.LEFT,
+        Direction.RIGHT: Direction.DOWN,
+        Direction.DOWN: Direction.RIGHT,
+        Direction.LEFT: Direction.UP
+    }
+}
+
+beam_splitters = {
+    '|': {
+        Direction.UP: (Direction.UP,),
+        Direction.RIGHT: (Direction.UP, Direction.DOWN),
+        Direction.DOWN: (Direction.DOWN,),
+        Direction.LEFT: (Direction.DOWN, Direction.UP)
+    },
+    '-': {
+        Direction.UP: (Direction.LEFT, Direction.RIGHT),
+        Direction.RIGHT: (Direction.RIGHT,),
+        Direction.DOWN: (Direction.RIGHT, Direction.LEFT),
+        Direction.LEFT: (Direction.LEFT,)
+    }
+}
+
+class Beam:
+    def __init__(self, i, j, direction):
+        self.i = i
+        self.j = j
+        self.direction = direction
+        
+    def move(self):
+        vector = self.direction.to_vector()
+        self.i += vector[0]
+        self.j += vector[1]
+
+def main():
+    with open(input) as f:
+        lines = f.readlines()
+    
+    contraption = [line.strip() for line in lines]
+    n = len(contraption)
+    m = len(contraption[0])
+    beams_grid = [[[False for _ in Direction] for _ in range(m)] for _ in range(n)]
+    
+    beams_to_check = deque()
+    beams_to_check.append(Beam(0, 0, Direction.RIGHT))
+    while len(beams_to_check) > 0:
+        beam = beams_to_check.popleft()
+        if beam.i < 0 or beam.i >= n or beam.j < 0 or beam.j >= m:
+            continue
+        if beams_grid[beam.i][beam.j][beam.direction.value]:
+            continue
+        
+        beams_grid[beam.i][beam.j][beam.direction.value] = True
+        
+        if contraption[beam.i][beam.j] in beam_splitters:
+            for direction in beam_splitters[contraption[beam.i][beam.j]][beam.direction]:
+                new_beam = Beam(beam.i, beam.j, direction)
+                new_beam.move()
+                beams_to_check.append(new_beam)
+        else:
+            if contraption[beam.i][beam.j] in beam_mirrors:
+                beam.direction = beam_mirrors[contraption[beam.i][beam.j]][beam.direction]
+            beam.move()
+            beams_to_check.append(beam)
+        
+    energized_tiles = 0
+    for row in beams_grid:
+        for tile in row:
+            if any(tile):
+                energized_tiles += 1
+    print(energized_tiles)
+    
+    
+if __name__ == '__main__':
+    main()
